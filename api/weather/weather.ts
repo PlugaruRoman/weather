11f91ac0f7b009d069d651b43e69a4ddd8028652
PlugaruRoman <97292989+PlugaruRoman@users.noreply.github.com>
2022-12-01 20:14:08 +0200

    fix api

--- api/weather/weather.ts
+++ api/weather/weather.ts
@@ -6,9 +6,7 @@ export const WeatherService = {
   getWeather: async (): Promise<IWeather> =>
     axios
       .get(
-        `/data/2.5/forecast?lat=${47.00367}&lon=${28.907089}&cnt=8&appid=${
-          process.env.NEXT_PUBLIC_WEATHER_KEY
-        }&units=metric`
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${47.00367}&lon=${28.907089}&cnt=8&appid=89513552b4f105442d50d8ea3bea505d&units=metric`
       )
       .then((res) => res.data),
 };
